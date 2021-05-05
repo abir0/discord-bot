3c8a181f1a8505d5c2b74162615c20ceb9d357c4
Abir Hasan <abir.spsc@gmail.com>
2021-05-05 20:28:47 +0600

    Major update

--- bot.py
+++ bot.py
@@ -1,43 +1,62 @@
 import discord
+from discord.ext import commands
 import os
-import random
 
-client = discord.Client()
+import random
+import datetime
+from urllib import parse, request
+import re
+
+bot = commands.Bot(command_prefix='$', description="This is a Helper Bot")
+
+greetings_list = ['Hey {}', 'Good day, {}!', 'Greetings {}']
 
 quotes_list = [("Jett", "Wash dish."),
                ("Sova", "I'm da hunter.")]
 
-def toss():
-    return random.choice(['Head', 'Tail'])
+## Commands
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
-def greet(man):
-    return random.choice(['Hey @{} .', 'Good day, @{} !', 'Greetings @{} .']).format(man)
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello, I\'m robot!')
 
-def generate_quote():
-    global quotes_list
-    q = random.choice(quotes_list)
-    return '\n"{}" \t ~ {}\n'.format(q[1], q[0])
+@bot.command()
+async def say(ctx, *words):
+    await ctx.send(" ".join(list(words)))
 
-@client.event
+@bot.command()
+async def greet(ctx, name):
+    await ctx.send(random.choice().format(name))
+
+@bot.command()
+async def quote(ctx):
+    quote = random.choice(quotes_list)
+    await ctx.send('\n"{}"\t ~ {}\n'.format(quote[1], quote[0]))
+
+@bot.command()
+async def toss(ctx):
+    embed = discord.Embed(color=discord.Color.blue())
+    url = random.choice(["https://i.imgur.com/csSP4ce.jpg", "https://i.imgur.com/NSrQtWx.jpg"])
+    embed.set_image(url=url)
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def info(ctx):
+    embed = discord.Embed(title=f"{ctx.guild.name}", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
+    embed.add_field(name="Server created at", value=f"{ctx.guild.created_at}")
+    embed.add_field(name="Server Owner", value=f"{ctx.guild.owner}")
+    embed.add_field(name="Server Region", value=f"{ctx.guild.region}")
+    embed.add_field(name="Server ID", value=f"{ctx.guild.id}")
+    embed.set_thumbnail(url="https://i.imgur.com/Bz4y6gC.png")
+    await ctx.send(embed=embed)
+
+## Events
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello, I\'m robot!')
-
-    if message.content.startswith('$quote'):
-        await message.channel.send(generate_quote())
-
-    if message.content.startswith('$toss'):
-        await message.channel.send(toss())
-
-    if message.content.startswith('$greet'):
-        await message.channel.send(greet(message.content.replace('$greet', '').strip()))
+    print('We have logged in as {0.user}'.format(bot))
 
 if __name__ == '__main__':
     client.run(os.environ['token'])
