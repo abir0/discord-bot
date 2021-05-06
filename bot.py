9d30f305bcd4b72f9b0c6ae86b1e72d90fcf311f
Abir Hasan <abir.spsc@gmail.com>
2021-05-06 16:37:33 +0600

    Update quote command

--- bot.py
+++ bot.py
@@ -5,23 +5,21 @@ import os
 from datetime import datetime
 from urllib.request import urlopen
 import random
+import json
 import re
 
-bot = commands.Bot(command_prefix='$', description="This is a Helper Bot")
+bot = commands.Bot(command_prefix="$", description="This is a Helper Bot")
 
-greetings_list = ['Hey {}', 'Good day, {}!', 'Greetings {}']
-
-quotes_list = [("Jett", "Wash dish."),
-               ("Sova", "I'm da hunter.")]
+greetings_list = ["Hey {}", "Good day, {}!", "Greetings {}"]
 
 ## Commands
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.send("pong")
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send('Hello, I\'m robot!')
+    await ctx.send("Hello, I\'m robot!")
 
 @bot.command()
 async def say(ctx, *words):
@@ -33,8 +31,9 @@ async def greet(ctx, name):
 
 @bot.command()
 async def quote(ctx):
-    quote = random.choice(quotes_list)
-    await ctx.send('\n"{}"\t ~ {}\n'.format(quote[1], quote[0]))
+    response = urlopen("https://zenquotes.io/api/random")
+    json_data = json.loads(response.read())
+    await ctx.send("\"{}\" ~{}".format(json_data[0]["q"], json_data[0]["a"]))
 
 @bot.command()
 async def toss(ctx):
@@ -58,24 +57,24 @@ async def info(ctx):
 
 @bot.command()
 async def youtube(ctx, *search):
-    query_string = '+'.join(list(search))
-    html_content = urlopen('https://www.youtube.com/results?search_query=' + query_string)
-    search_results = re.search(r'\"\/watch\?v=(\S{11})\"', html_content.read().decode())
+    query_string = "+".join(list(search))
+    html_content = urlopen("https://www.youtube.com/results?search_query=" + query_string)
+    search_results = re.search(r"\"\/watch\?v=(\S{11})\"", html_content.read().decode())
     # shows the first result
-    await ctx.send('https://www.youtube.com/watch?v=' + search_results[1])
+    await ctx.send("https://www.youtube.com/watch?v=" + search_results[1])
 
 ## Events
 @bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
+    print("We have logged in as {0.user}".format(bot))
 
 @bot.listen()
 async def on_message(message):
     if message.author == bot.user:
         return
 
-    if 'hi' in message.content.lower():
-        await message.channel.send('hi')
+    if "hi" in message.content.lower():
+        await message.channel.send("hi")
 
-if __name__ == '__main__':
-    bot.run(os.environ['token'])
+if __name__ == "__main__":
+    bot.run(os.environ["token"])
